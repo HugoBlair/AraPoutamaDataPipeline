8238ed56914aa15bbda0f083629af786c55cf59c
Hugo <hugorosa123@gmail.com>
2024-10-08 21:59:56 +1300

    Added AWS ingestion ***NON-FUNCTIONAL***

--- P03_Pull_From_S3_to_Raw.R
+++ P03_Pull_From_S3_to_Raw.R
@@ -17,10 +17,44 @@ library(tidyr)
 library(readxl)
 library(dplyr)
 library(purrr)
+library("aws.s3")
+library(lubridate)
 
 #############################################
 ### Pull from S3 into dataframe         #####
 #############################################
+
+# Load configuration
+config <- config::get(file = "config.yml")
+
+# Set AWS credentials
+Sys.setenv(
+  "AWS_ACCESS_KEY_ID" = config$aws$AWS_ACCESS_KEY_ID,
+  "AWS_SECRET_ACCESS_KEY" = config$aws$AWS_SECRET_ACCESS_KEY,
+  "AWS_DEFAULT_REGION" = config$aws$AWS_DEFAULT_REGION
+)
+
+#viewing stored data files
+data.table::rbindlist(get_bucket(bucket = "ara-poutama-data"))
+
+load_data_from_s3 <- function() {
+  bucket <- "ara-poutama-data"
+  file_name <- "df_data_scd2.csv"
+  
+  
+  if (aws.s3::object_exists(object = file_name, bucket = bucket)) {
+    s3_object <- aws.s3::get_object(object = file_name, bucket = bucket)
+    raw_data <- read.csv(text = rawToChar(s3_object))
+    return(raw_data)
+  } else {
+    stop("File not found in S3 bucket")
+  }
+}
+
+# use the s3 URI
+#df = s3read_using(FUN = read.csv, object = "s3://ara-poutama-data/df_data_scd2.csv")   
+
+df = load_data_from_s3()
 
 
 # Simulating for now, pull from local excel
